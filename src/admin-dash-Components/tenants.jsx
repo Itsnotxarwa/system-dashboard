bdf132e5b2718b84cfd29a4cb4b7263658999c6d
itsnotxarwa <marwa.kchkh@gmail.com>
2026-03-06 17:14:51 +0100

    add export and add new tenant buttons + change the color of dashboard to syne

--- src/admin-dash-Components/tenants.jsx
+++ src/admin-dash-Components/tenants.jsx
@@ -1,13 +1,28 @@
+import { ArrowDownToLine, Plus } from "lucide-react";
 import KpiCards from "./kpiCards";
 
 export default function Tenants() {
     return (
     <div className="p-4">
-        <div className="flex justify-start items-start gap-2 mb-4">
-            <h1 className=" mb-4 font-syne text-[22px] font-bold tracking-[-0.5px]">Tenants</h1>
-            <p>/Overview</p>
+        <div className="flex items-center justify-between mb-4">
+            <div className="flex tenant-title justify-start items-start gap-2 mt-4">
+                <h1 className=" mb-4 text-[22px] font-bold tracking-[-0.5px]">Tenants</h1>
+                <p className="text-sm font-medium">/Overview</p>
+            </div>
+            <div>
+                <button className="py-2 px-4 border border-gray-300 text-gray-400 
+                cursor-pointer rounded-md mr-2">
+                    <ArrowDownToLine size={16} className="inline mr-1" />
+                    Export
+                </button>
+                <button className="py-2 px-4 bg-[#032ca6] text-white rounded-md
+                cursor-pointer hover:bg-[#032ca6]/90 transition-colors duration-300">
+                    <Plus size={16} className="inline mr-1" />
+                    Add Tenant
+                </button>
+            </div>
         </div>
-        <div className="py-6">
+        <div className="py-2">
             <KpiCards />
         </div>
     </div>
