5a5cf59c5bf7665c0572b63de70619638aaf8f4b
itsnotxarwa <marwa.kchkh@gmail.com>
2026-03-17 13:47:18 +0100

    handle token, role, and userId in localStorage

--- src/admin-dash-Components/admin-dashboard.jsx
+++ src/admin-dash-Components/admin-dashboard.jsx
@@ -4,6 +4,15 @@ import Sidebar from "./sidebar";
 import Tenants from "./tenants";
 
 export default function AdminDashboard() {
+  const params = new URLSearchParams(window.location.search);
+  const token = params.get("token");
+  const role = params.get("role");
+  const userId = params.get("userId");
+  
+  localStorage.setItem("token", token);
+  localStorage.setItem("role", role);
+  localStorage.setItem("userId", userId);
+
   return (
     <div className="flex min-h-screen bg-white text-black">
       <Sidebar />
