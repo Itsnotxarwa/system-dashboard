dbc6295211909f6cfe0bb252657eb93e81655c6e
itsnotxarwa <marwa.kchkh@gmail.com>
2026-03-29 14:14:54 +0100

    delete teanantDetails import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AdminDashboard from "./admin-dash-Components/admin-dashboard"
-import TenantDetails from './admin-dash-Components/tenantDetails/tenantDetails';
 import Agents from './admin-dash-Components/tenantDetails/Agents';
 import CallRecords from './admin-dash-Components/tenantDetails/CallRecords';
 
@@ -11,7 +10,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<AdminDashboard />}/>
-        <Route path='/tenants/:id' element={<TenantDetails />} />
         <Route path="/tenant/:id/agents" element={<Agents />} />
         <Route path="/tenant/:id/call-records" element={<CallRecords />} />
       </Routes>
