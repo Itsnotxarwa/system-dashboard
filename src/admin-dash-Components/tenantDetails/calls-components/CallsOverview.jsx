0c7db48122f12d669ba98024f2e73956cc2d6fd4
itsnotxarwa <marwa.kchkh@gmail.com>
2026-05-25 18:47:03 +0100

    update

--- src/admin-dash-Components/tenantDetails/calls-components/CallsOverview.jsx
+++ src/admin-dash-Components/tenantDetails/calls-components/CallsOverview.jsx
@@ -11,6 +11,20 @@ export default function CallsOverview({tenant, calls, callSessions, onChange}) {
     const [range, setRange] = useState("30");
     const [openDrawer, setOpenDrawer] = useState(false);
     const [selectedSession, setSelectedSession] = useState(null)
+
+        if (!calls) {
+            return (
+            <div className="flex items-center justify-center h-64">
+                <svg className="w-[3.25em] origin-center animate-[spin_2s_linear_infinite]" 
+                viewBox="25 25 50 50">
+                    <circle
+                    className="loading-circle" 
+                    r="20" cy="50" cx="50"></circle>
+                </svg>
+            </div>
+            );
+        }
+    
     return(
         <div className="min-h-screen">
             <div className="max-w-7xl mx-auto p-6">
