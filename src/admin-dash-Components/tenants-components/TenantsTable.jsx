f08076d2c1a309aba9ab0525b5c7ec7decbf6efa
itsnotxarwa <marwa.kchkh@gmail.com>
2026-05-27 11:56:11 +0100

    update

--- src/admin-dash-Components/tenants-components/TenantsTable.jsx
+++ src/admin-dash-Components/tenants-components/TenantsTable.jsx
@@ -5,7 +5,7 @@ export default function TenantsTable({filteredTenants, handleEdit, handleDelete}
     const navigate = useNavigate();
     
     return(
-        <div className="bg-[#0d1117] border border-[#21262d] rounded-xl overflow-hidden">
+        <div className="flex flex-col bg-[#161b22] border border-[#21262d] rounded-[10px] overflow-hidden">
             <table className="w-full border-collapse">
                 <thead>
                     <tr className="border-b border-[#21262d]">
