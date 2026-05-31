444f42c0d35480279cb2ba4ebf136a94230fbf91
itsnotxarwa <marwa.kchkh@gmail.com>
2026-05-31 16:05:45 +0100

    update

--- src/admin-dash-Components/metrics-components/SpotlightChart.jsx
+++ src/admin-dash-Components/metrics-components/SpotlightChart.jsx
@@ -14,7 +14,9 @@ export default function SpotlightChart({ spotlight, loading }) {
         },
     ];
 
-    if (!loading) {
+    const COLORS = ["#3fb950", "#f85149"];
+
+    if (loading) {
         return (
             <div className="flex items-center justify-center h-64">
                 <svg className="w-[3.25em] origin-center animate-[spin_2s_linear_infinite]" 
@@ -33,12 +35,11 @@ export default function SpotlightChart({ spotlight, loading }) {
                 <XAxis dataKey="name" />
                 <YAxis />
                 <Tooltip />
-                <Bar
-                dataKey="value"
-                fill={({ name }) =>
-                    name.includes("Best") ? "#39d3bb" : "#f85149"
-                }
-                />
+                <Bar dataKey="value">
+                    {data.map((_, index) => (
+                        <Cell key={index} fill={COLORS[index]} />
+                    ))}
+                </Bar>
             </BarChart>
         </ResponsiveContainer>
     );
