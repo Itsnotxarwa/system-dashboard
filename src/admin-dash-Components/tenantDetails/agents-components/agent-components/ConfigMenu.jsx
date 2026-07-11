5fe14fe8fa313c40505fe8e79d1a393c4904d468
itsnotxarwa <marwa.kchkh@gmail.com>
2026-07-11 20:47:45 +0100

    update

--- src/admin-dash-Components/tenantDetails/agents-components/agent-components/ConfigMenu.jsx
+++ src/admin-dash-Components/tenantDetails/agents-components/agent-components/ConfigMenu.jsx
@@ -1,4 +1,4 @@
-import { Columns3Cog, SquareTerminal, Mic, Wrench } from "lucide-react"
+import { Columns3Cog, SquareTerminal, Mic, Wrench, ChevronRight } from "lucide-react"
 
 export default function ConfigMenu({section, setSection}) {
     const configItems = [
@@ -8,17 +8,18 @@ export default function ConfigMenu({section, setSection}) {
         {key: "Voice", label: "Voice", icon: Mic},
     ]
     return(
-        <div className="w-52 h-60 shrink-0 border-r rounded-[5px] border-[#243055] bg-[#0F162B] shadow-[0_10px_40px_rgba(14,99,221,0.18)] overflow-y-auto py-2">
+        <div className="w-52 h-1/2 shrink-0 border border-[#8b949e] overflow-hidden py-6 px-2">
             {configItems.map(item => (
                 <div key={item.key}>
                     <button
                     onClick={() => {
                         setSection(item.key);
                     }}
-                    className={`w-full flex items-start gap-3 px-4 py-2 text-sm font-medium transition-all duration-300
+                    className={`w-full flex items-start gap-3 px-4 py-2 text-sm font-medium transition-all duration-300 overflow-hidden text-left cursor-pointer
                     ${section===item.key ? "text-[#58a6ff] font-medium bg-[rgba(88,166,255,.12)]" : "hover:bg-[#21262d] hover:scale-105 hover:text-[#e6edf3]"}`}>
                         <item.icon size={13} stroke={section===item.key ? "#58a6ff" : "currentColor"} />
                         <span className="flex-1 text-left">{item.label}</span>
+                        <ChevronRight size={12} stroke={section===item.key ? "#58a6ff" : "currentColor"} />
                     </button>
                 </div>
             ))}
