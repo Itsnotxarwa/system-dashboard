4c763c21d111dcdac8d4f68d112f53526811c21e
itsnotxarwa <marwa.kchkh@gmail.com>
2026-07-17 18:17:46 +0100

    update

--- src/admin-dash-Components/tenantDetails/agents-components/agent-components/actions-components/ActionDuringCall.jsx
+++ src/admin-dash-Components/tenantDetails/agents-components/agent-components/actions-components/ActionDuringCall.jsx
@@ -19,26 +19,28 @@ export default function ActionDuringCall({setOpenCreateCallTransfer, callTransfe
                     addLabel="Add an action" />
                 ) : (
                     callTransferActions?.map((action, index) => (
-                        <ActionRow
-                            key={index}
-                            icon={<PhoneOff size={16} className="text-white" />}
-                            iconBg="bg-[#d29922]"
-                            name={action?.name || ""}
-                            description={action?.description || ""}
-                        />
+                        <div>
+                            <ActionRow
+                                key={index}
+                                icon={<PhoneOff size={16} className="text-white" />}
+                                iconBg="bg-[#d29922]"
+                                name={action?.name || ""}
+                                description={action?.description || ""}
+                            />
+                            <div className="flex justify-center">
+                                <button 
+                                onClick={() => setOpenCreateCallTransfer(true)}
+                                className="cursor-pointer flex items-center gap-1.5 text-sm px-6 py-2.5 rounded-lg 
+                                text-[#58a6ff] hover:bg-[rgba(88,166,255,.08)] border border-[rgba(88,166,255,.25)]
+                                transition-colors font-mono">
+                                    <span className="text-[#58a6ff] text-base leading-none">
+                                        <Plus size={14} />
+                                    </span> Add Action
+                                </button>
+                            </div>
+                        </div>
                     ))
                 )}
-            </div>
-            <div className="flex justify-center">
-                <button 
-                onClick={() => setOpenCreateCallTransfer(true)}
-                className="cursor-pointer flex items-center gap-1.5 text-sm px-6 py-2.5 rounded-lg 
-                text-[#58a6ff] hover:bg-[rgba(88,166,255,.08)] border border-[rgba(88,166,255,.25)]
-                transition-colors font-mono">
-                    <span className="text-[#58a6ff] text-base leading-none">
-                        <Plus size={14} />
-                    </span> Add Action
-                </button>
             </div>
         </div>
     )
